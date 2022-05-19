8d4dbf0d9662028c59af7ffe6b9be9089f45ab66
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-05-19 21:50:13 +1200

    added javascript to make the font-page movements

--- Project01_Fetching_APIs/script.js
+++ Project01_Fetching_APIs/script.js
@@ -1,12 +1,33 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Quotes are fetch with API
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
+
     // Pick a random quot from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
-}
 
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
 
 // Get Quotes From API
 async function getQuotes() {
@@ -20,5 +41,28 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function twitterQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+
+twitterBtn.addEventListener('click', tweetQuote)
+
 // On Load
 getQuotes();
+
+//****************************************************************************
+// Quotes fetch from local file "quotes.js"
+// // Show New Quote
+// function newQuote() {
+//     // Pick a random quote from apiQuotes array
+//     // localQuotes is the function name from quotes.js file
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//     console.log(quote);
+// }
+
+// newQuote();
