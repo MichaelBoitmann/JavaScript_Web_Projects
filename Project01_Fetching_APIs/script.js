7aa5f61f7f5acd667bb8ff9776f73fe78945960e
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-05-22 08:33:13 +1200

    remodelling the script function

--- Project01_Fetching_APIs/script.js
+++ Project01_Fetching_APIs/script.js
@@ -1,47 +1,29 @@
-const quoteContainer = document.getElementById('quote-container');
+const quote = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Quotes are fetch with API
-let apiQuotes = [];
-
-// Show Loading
-function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-}
-
-// Hide Loading
-function complete() {
-    quoteContainer.hidden = false;
-    loader.hidden - true;
-}
+let apiQuotes = []
 
 // Show New Quote
 function newQuote() {
-    loading();
-    // Pick a random quot from apiQuotes array
+    // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
-    // Check if Author field is blank and replace it with 'Unknown'
+    // Check if Author field is blank and replace it with 'unknow'
     if (!quote.author) {
         authorText.textContent = 'Unknown';
     } else {
         authorText.textContent = quote.author;
     }
-
-    // Check Quote length to determine styling
-    if (quote.text.length > 100) {
+    // Check Quote length to determine styling which is the font size
+    if (quote.text.length > 50) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
-    // Set Quote, Hide Loader
-    quoteText.textContent = quote.text;
-    complete();
+    quoteText.textContent = quote.text;    
 }
 
 // Get Quotes From API
@@ -51,7 +33,7 @@ async function getQuotes() {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
-    } catch (error) {
+    } catch  (error) {
         // Catch Error Here
     }
 }
@@ -59,29 +41,27 @@ async function getQuotes() {
 // Tweet Quote
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    // '_blank' will allow the twitter to open in a new tab
     window.open(twitterUrl, '_blank');
 }
 
-// Event Listener
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
-
-twitterBtn.addEventListener('click', tweetQuote)
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-// getQuotes();
-loading();
+getQuotes();
 
 
 
 
 
+//############################################################################
+// Showing New Quote locally from quote.js which comes on this project folder
+// You can see the quotes from console.log
 
-//****************************************************************************
-// Quotes fetch from local file "quotes.js"
-// // Show New Quote
 // function newQuote() {
 //     // Pick a random quote from apiQuotes array
-//     // localQuotes is the function name from quotes.js file
 //     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 //     console.log(quote);
 // }
