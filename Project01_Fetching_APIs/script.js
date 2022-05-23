9d5aff8d5bcf8d59a41af3ded6418376adf3dcfe
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-05-23 22:10:23 +1200

    finalize the project that will load the APIs and twitter quotes

--- Project01_Fetching_APIs/script.js
+++ Project01_Fetching_APIs/script.js
@@ -1,52 +1,98 @@
-const quote = document.getElementById('quote-container');
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-let apiQuotes = []
+// let apiQuotes = []
 
-// Show New Quote
-function newQuote() {
-    // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Check if Author field is blank and replace it with 'unknow'
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    // Check Quote length to determine styling which is the font size
-    if (quote.text.length > 50) {
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Get Quote From API
+async function getQuote() {
+  loading();
+  const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    // If Author is blank, add 'Unknown'
+      if (data.quoteAuthor === '') {
+        authorText.innerText = 'Unknown';
+      } else {
+        authorText.innerText = data.quoteAuthor;
+      }
+      // Reduce font size for long quotes
+      if (data.quoteText.length > 120) {
         quoteText.classList.add('long-quote');
-    } else {
+      } else {        
         quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent = quote.text;    
+      }
+      quoteText.innerText = data.quoteText;
+      // Stop Loader, Show Quote
+      complete();
+  } catch (error) {
+    getQuote();
+  }
 }
 
-// Get Quotes From API
-async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch  (error) {
-        // Catch Error Here
-    }
-}
+// // Show New Quote
+// function newQuote() {
+//     loading();
+//     // Pick a random quote from apiQuotes array
+//     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//     // Check if Author field is blank and replace it with 'unknow'
+//     if (!quote.author) {
+//         authorText.textContent = 'Unknown';
+//     } else {
+//         authorText.textContent = quote.author;
+//     }
+//     // Check Quote length to determine styling which is the font size
+//     if (quote.text.length > 80) {
+//         quoteText.classList.add('long-quote');
+//     } else {
+//         quoteText.classList.remove('long-quote');
+//     }
+//     // Set Quote, Hide Loader
+//     quoteText.textContent = quote.text;  
+//     complete();
+// }
+
+// // Get Quotes From API
+// async function getQuotes() {
+//     loading();
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch  (error) {
+//         // Catch Error Here
+//     }
+// }
 
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     // '_blank' will allow the twitter to open in a new tab
     window.open(twitterUrl, '_blank');
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', newQuote);
+newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
