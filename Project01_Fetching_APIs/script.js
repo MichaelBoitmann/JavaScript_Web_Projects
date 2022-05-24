68cf82b0359ec28e0ae3681659d0ee421800c858
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-05-24 23:24:32 +1200

    image loading script

--- Project01_Fetching_APIs/script.js
+++ Project01_Fetching_APIs/script.js
@@ -3,17 +3,17 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const spinner = document.getElementById('loader');
 
 function showLoadingSpinner() {
-  loader.hidden = false;
+  spinner.hidden = false;
   quoteContainer.hidden = true;
 }
 
 function removeLoadingSpinner() {
-  if (!loader.hidden) {
+  if (!spinner.hidden) {
     quoteContainer.hidden = false;
-    loader.hidden = true;
+    spinner.hidden = true;
   }
 }
 
