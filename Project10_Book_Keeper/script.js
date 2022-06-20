36d3a151ce11c30b2ba23f508cd9e9a437bf5f9b
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-06-20 22:47:09 +1200

    enhanced the website name and url name

--- Project10_Book_Keeper/script.js
+++ Project10_Book_Keeper/script.js
@@ -6,37 +6,77 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarkContainer = document.getElementById('bookmarks-container');
 
-// Show MOdal, Focus on Input
+let bookmarks = [];
+
+// Show Modal, Focus on Input
 function showModal() {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
 }
 
-// MOdal Event Listeners
+// Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // Valid
+    return true;
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+    // Get bookmarks from localStorage if available
+    if (localStorage.getItem('bookmarks')) {
+        boookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name: "Boitmann Design",
+                url: "https://www.linkedin.com/in/michael-boitmann-85905a33/",
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
 
 // Handle Data from Form
 function storeBookmark(e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
     let urlValue = websiteUrlEl.value;
-    console.log(nameValue, urlValue);
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+    }
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 // Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
 
-
-
-
-// // Old Version
-// if (!urlValue.includes('http://', 'https://')) {     
-//     urlValue = `https://${urlValue}`; 
-// } 
- 
-// // New Version
-// if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
-//      urlValue = `https://${urlValue}`; 
-// }
+// Lon Load, Fetch Bookmarks
+fetchBookmarks();
