f5927da3c41c56f41af39f56b39af1f71e722a9c
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-06-23 23:17:23 +1200

    added script for progress bar and to click anywhere in the bar

--- Project11_Video_Player/script.js
+++ Project11_Video_Player/script.js
@@ -9,7 +9,6 @@ const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
-
 function showPlayIcon() {
     playBtn.classList.replace('fa-pause', 'fa-play');
     playBtn.setAttribute('title', 'Play');
@@ -29,11 +28,31 @@ function togglePlay() {
 // On Video End, show play button icon
 video.addEventListener('ended', showPlayIcon);
 
-
-
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;    
+}
+
+
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`
+    video.currentTime = newTime * video.duration;
+    console.log(e);
+}
 
 // Volume Controls --------------------------- //
 
@@ -49,4 +68,6 @@ video.addEventListener('ended', showPlayIcon);
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
-
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
