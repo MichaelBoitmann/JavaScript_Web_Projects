8efac8c20e3129d1fca9f9b6e0d8724262c37abf
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-03 10:47:34 +1200

    constructed the player and computer selection on script using IDs of each item

--- Project13_Spock_Rock_Game/script.js
+++ Project13_Spock_Rock_Game/script.js
@@ -1,4 +1,20 @@
+const playerScoreEl = document.getElementById('playerScore');
+const playerChoiceEl = document.getElementById('playerChoice');
+const computerScoreEl = document.getElementById('ComputerScore');
+const computerChoiceEl = document.getElementById('ComputerChoice');
+const resultText = document.getElementById('resultText');
 
+const playerRock = document.getElementById('playerRock');
+const playerPaper = document.getElementById('playerPaper');
+const playerScissors = document.getElementById('playerScissors');
+const playerLizard = document.getElementById('playerLizard');
+const playerSpock = document.getElementById('playerSpock');
+
+const computerRock = document.getElementById('computerRock');
+const computerPaper = document.getElementById('computerPaper');
+const computerScissors = document.getElementById('computerScissors');
+const computerLizard = document.getElementById('computerLizard');
+const computerSpock = document.getElementById('computerSpock');
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
