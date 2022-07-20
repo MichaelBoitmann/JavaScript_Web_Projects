d6ca9db7aebef4a433bb1292c7be0025fe7532a6
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-20 21:58:17 +1200

    set some constants that are needed for the rest of the project

--- Project15_Math_Sprint_Game/script.js
+++ Project15_Math_Sprint_Game/script.js
@@ -3,12 +3,36 @@ const gamePage = document.getElementById('game-page');
 const scorePage = document.getElementById('score-page');
 const splashPage = document.getElementById('splash-page');
 const countdownPage = document.getElementById('countdown-page');
+
 // Splash Page
+const startForm = document.getElementById('start-form');
+const radioContainer = document.querySelectorAll('.radio-container')
+const radioInputs = document.querySelectorAll('input');
+const bestScores = document.querySelectorAll('.best-score-value');
 
 // Countdown Page
+const countdown = document.querySelector('.countdown');
 
 // Game Page
+const itemContainer = document.querySelector('.item-container');
 
 // Score Page
+const finalTimeEl = document.querySelector('.final-time');
+const baseTimeEl = document.querySelector('.base-time');
+const penaltyTimeEl = document.querySelector('.penalty-time');
+const playAgainBtn = document.querySelector('.play-again');
 
 // Equations
+let equationsArray = [];
+
+// Game Page
+let firstNumber = 0;
+let secondNumber = 0;
+let equationObject = {};
+const wrongFormat = [];
+
+// Time
+
+// Scroll
+
+// Create
