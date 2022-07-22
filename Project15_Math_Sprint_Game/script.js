ca98af85390ef670fb1d0b6ae6171a5e0be2bb2a
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-22 22:36:28 +1200

    added the equationTo DOM function

--- Project15_Math_Sprint_Game/script.js
+++ Project15_Math_Sprint_Game/script.js
@@ -32,6 +32,97 @@ let secondNumber = 0;
 let equationObject = {};
 const wrongFormat = [];
 
+// Time
+
+// Scroll
+
+// Dipslay Game Page
+function showGamePage() {
+    gamePage.hidden = false;
+    countdownPage.hidden = true;
+}
+
+// Get Random Number up to a max number
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Create Correct/Incorrect Random Equations
+function createEquations() {
+    // Randomly choose how many correct equations there should be
+    const correctEquations = getRandomInt(questionAmount);
+    console.log('correct equations: ', correctEquations);
+    
+    // Set amount of wrong equations
+    const wrongEquations = questionAmount - correctEquations;
+    console.log('wrong equations: ', wrongEquations);
+    
+    // Loop through, multiply random numbers up to 9, push to array
+    for (let i = 0; i < correctEquations; i++) {
+      firstNumber = getRandomInt(9);
+      secondNumber = getRandomInt(9);
+      const equationValue = firstNumber * secondNumber;
+      const equation = `${firstNumber} x ${secondNumber} = ${equationValue}`;
+      equationObject = { value: equation, evaluated: 'true' };
+      equationsArray.push(equationObject);
+    }
+    
+    // Loop through, mess with the equation results, push to array
+    for (let i = 0; i < wrongEquations; i++) {
+      firstNumber = getRandomInt(9);
+      secondNumber = getRandomInt(9);
+      const equationValue = firstNumber * secondNumber;
+      wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
+      wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+      wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
+      const formatChoice = getRandomInt(3);
+      const equation = wrongFormat[formatChoice];
+      equationObject = { value: equation, evaluated: 'false' };
+      equationsArray.push(equationObject);
+    }
+    shuffle(equationsArray);
+    equationsToDOM();
+  }
+
+  // Add Equations to DOM
+  function equationsToDOM() {
+    equationsArray.forEach((equation) => {
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // Equation Text
+        const equationText = document.createElement('h1');
+        equationText.textContent = equation.value;
+        // Append
+        item.appendChild(equationText);
+        itemContainer.appendChild(item);
+    });
+  }
+  
+  // Dynamically adding correct/incorrect equations
+  function populateGamePage() {
+    // Reset DOM, Set Blank Space Above
+    itemContainer.textContent = '';
+    // Spacer
+    const topSpacer = document.createElement('div');
+    topSpacer.classList.add('height-240');
+    // Selected Item
+    const selectedItem = document.createElement('div');
+    selectedItem.classList.add('selected-item');
+    // Append
+    itemContainer.append(topSpacer, selectedItem);
+  
+    // Create Equations, Build Elements in DOM
+    createEquations();
+    equationsToDOM();
+  
+    // Set Blank Space Below
+    const bottomSpacer = document.createElement('div');
+    bottomSpacer.classList.add('height-500');
+    itemContainer.appendChild(bottomSpacer);
+  }
+
+
 // Display 3, 2, 1 Go
 function countdownStart() {
     countdown.textContent = '3';
@@ -51,6 +142,8 @@ function showCountdown() {
     countdownPage.hidden = false;
     splashPage.hidden = true;
     countdownStart();
+    populateGamePage();
+    setTimeout(showGamePage, 400);
 }
 
 // Get the value from selected radio button
