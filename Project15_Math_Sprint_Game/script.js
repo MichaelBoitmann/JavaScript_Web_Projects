0a62e882a52367af5203f3d57872f6c95a063ed2
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-24 23:51:48 +1200

    created a start timer when game is clicked

--- Project15_Math_Sprint_Game/script.js
+++ Project15_Math_Sprint_Game/script.js
@@ -39,23 +39,39 @@ let timePlayed = 0;
 let baseTime =0;
 let penaltyTime = 0;
 let finalTime = 0;
-let finaldTime = '0.0s';
-
+let finalTimeDisplay = '0.0s';
 
 // Scroll
 let valueY = 0;
 
-// Start timer when game is clicked
+// Stop Timer, Process REsults, got to Score Page
+function checkTime() {
+    console.log(timePlayed);
+    if (playerGuessArray.length = questionAmount) {
+        console.log('player guess array: ', playerGuessArray);
+        clearInterval(timer);
+    }
+}
+
+// Add a tenth of a second to timePlayed
+function addTime() {
+    timePlayed += 0.1;
+    checkTime();
+}
+
+// Start timer when game pages is clicked
 function startTimer() {
     // Reset times
     timePlayed = 0;
     penaltyTime = 0;
     finalTime = 0;
+    timer = setInterval(addTime, 100);
+    gamePage.removeEventListener('click', startTimer);
 }
 
 // Scroll, Store user selection in playerGuessArray
 function select(guessedTrue) {
-    console.log('player guess array: ', playerGuessArray);
+
     // Scroll 80 pixels
     valueY += 80;
     itemContainer.scroll(0, valueY);
