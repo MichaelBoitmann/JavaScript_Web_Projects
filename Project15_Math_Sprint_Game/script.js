0ac3f43b1de9bf2e2cb782a1cb7e887ebffe84ee
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-25 22:02:15 +1200

    added functions of reset game, score page, scores to DOM

--- Project15_Math_Sprint_Game/script.js
+++ Project15_Math_Sprint_Game/script.js
@@ -44,6 +44,40 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY = 0;
 
+// Reset Game
+function playAgain() {
+    gamePage.addEventListener('click', startTimer);
+    scorePage.hidden = true;
+    splashPage.hidden = false;
+    equationsArray = [];
+    playerGuessArray = [];
+    valueY = 0;
+    playAgainBtn.hidden = true;
+}
+
+// Show Score Page
+function showScorePage() {
+    // Show Play Again button after 1 second
+    setTimeout(() => {
+        playAgainBtn.hidden = false;
+    }, 1000);
+    gamePage.hidden = true;
+    scorePage.hidden = false;
+}
+
+// Format & Display Time in DOM
+function scoresToDOM() {
+    finalTimeDisplay = finalTime.toFixed(1);
+    baseTime = timePlayed.toFixed(1);
+    penaltyTime = penaltyTime.toFixed(1);
+    baseTimeEl.textContent = `Base Time: ${baseTime}s`;
+    penaltyTimeEl.textContent = `Penalty: +${penaltyTime}s`;
+    finalTimeEl.textContent = `${finalTimeDisplay}s`;
+    // Scroll to Top, got to Score Page
+    itemContainer.scrollTo({ top: 0, behavior: 'instant'});
+    showScorePage();
+}
+
 // Stop Timer, Process Results, got to Score Page
 function checkTime() {
     console.log(timePlayed);
@@ -61,6 +95,7 @@ function checkTime() {
         });
         finalTime = timePlayed + penaltyTime;
         console.log('time: ', timePlayed, 'penalty: ', penaltyTime, 'final: ', finalTime);
+        scoresToDOM();
     }
 }
 
