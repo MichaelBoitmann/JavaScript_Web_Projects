66ae6376c14d27e5a49bb6c99c43ea5bade58b9e
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-27 23:40:19 +1200

    initial commit for kanban board project with style and initial script

---
+++ Project16_Drag_n_Drop/script.js
@@ -0,0 +1,19 @@
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const saveItemBtns = document.querySelectorAll('.solid');
+const addItemContainers = document.querySelectorAll('.add-container');
+const addItems = document.querySelectorAll('.add-item');
+// Item Lists
+const itemLists = document.querySelectorAll('.drag-item-list');
+const backlogList = document.getElementById('backlog-list');
+const progressList = document.getElementById('progress-list');
+const completeList = document.getElementById('complete-list');
+const onHoldList = document.getElementById('on-hold-list');
+
+// Items
+
+
+// Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
