d14207fefec55a471659f02946c060730f25e332
MichaelBoitmann <michael_boitmann@yahoo.com>
2022-07-31 23:44:40 +1200

    added add to column, show input box, and hide input box functions

--- Project16_Drag_n_Drop/script.js
+++ Project16_Drag_n_Drop/script.js
@@ -12,7 +12,6 @@ const onHoldList = document.getElementById('on-hold-list');
 // Items
 let updatedOnLoad = false;
 
-
 // Initialize Arrays
 let backlogListArray = [];
 let progressListArray = [];
@@ -108,6 +107,31 @@ function updateDOM() {
   updatedOnLoad = true;
   updateSavedColumns();
 }
+
+// Add to Column List, Reset Textbox
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
 
 // Allows arrays to reflect Drag and Drop items
 function rebuildArrays () {
