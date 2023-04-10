aac6f783aaa280c3037e7b48ec9af47107fb45a0
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-04-10 23:24:08 +1200

    update the API call

--- Project01_Quote_Generator/script.js
+++ Project01_Quote_Generator/script.js
@@ -1,45 +1,62 @@
-// let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
-// Show New Quote
-function newQuote() {
-  // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // Check if Autho field is blank an dreplace it with 'Unknown'
-  if (!quote.author) {
-    authorText.textContent = 'Unknown';
-  } else {
-    authorText.textContent = quote.author;
-  }
-  // Chec Quote length to determine styling
-  if (quote.text.length >100) {
-    quoteText.classList.add('long-quote')
-  }
+let apiQuotes = [];
+
+// show showLoadingSpinner
+function showLoadingSpinner(){
+	loader.hidden = false;
+	quoteContainer.hidden = true;
 }
 
-// Get Quotes From API
+//  Hide Loder
+function removeLoadingSpinner(){
+	loader.hidden = true;
+	quoteContainer.hidden = false;
+}
+
+
+function newQuote() {
+	showLoadingSpinner();// pick a rondom quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author ? quote.author : "Unknown";
+  // quoteText.textContent = quote.text.length > 100 ? 'long-quote' = false : 'long-quote' = true;
+	quoteText.textContent = quote.text;
+
+  removeLoadingSpinner();
+}
+
+// Get Quotes from API
 async function getQuotes() {
-  const apiUrl = 'https://zenquotes.io/api/quotes';
+	showLoadingSpinner();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    // Catch Error here
+    // catch error
+    alert(error);
   }
 }
 
-
-// Tweet Quote
+//  Tweet Quote
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`; 
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
 }
 
-// On load
-// getQuotes();
-newQuote();
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
 
+// onLoad
 
-
+getQuotes();
 
 
 
